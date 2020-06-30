291910f6491b96097e37c0bec19fffb28fcee7df
Levy Adams <letitslide87@gmail.com>
2020-06-30 18:06:17 -0400

    asd

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ app.use('/', routes);
 app.use('/', passport.authenticate('jwt', { session : false }), secureRoutes);
 
 // catch all other routes
-app.use((req, res, next) => {
-  res.status(404).json({ message: '404 - Not Found' });
-});
+// app.use((req, res, next) => {
+//   res.status(404).json({ message: '404 - Not Found' });
+// });
 
 // handle errors
 app.use((err, req, res, next) => {
