8b6b7a2dcf067f6435940344ec66e6522b70ae01
Levy Adams <letitslide87@gmail.com>
2020-06-30 21:31:10 -0400

    dsa

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ router.get('/status', (req, res, next) => {
 });
 
 router.post('/signup', async (req, res, next) => {
-  return next(res.status(200).send({ message: 'signup successful' }));
+  return next(res.status(200).json({ message: 'signup successful' }));
 });
 
 
