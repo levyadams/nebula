1119110260a04373dcbfa233b8e8bf8920a4818b
Levy Adams <letitslide87@gmail.com>
2020-07-01 19:00:56 -0400

    asd

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ router.get('/status', (req, res, next) => {
 });
 
 router.post('/signup', passport.authenticate('signup', { session: false }), async (req, res, next) => {
-  return next(res.status(200).json({ message: 'signup successful' }));
+  return next(res.status(200));
 });
 // router.post('/signup', async (req, res, next) => {
 //   return next(res.status(200).json({ message: 'signup successful'}));
