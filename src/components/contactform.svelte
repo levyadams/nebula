1e1da7d626a3b1b41af8bde92e76d844efc832e0
levyadams <letitslide87@gmail.com>
2020-11-08 00:28:02 -0500

    asd

--- src/components/contactform.svelte
+++ src/components/contactform.svelte
@@ -1,30 +1,25 @@
 <form class="content">
-    <label>Nome
+    <p>
+        <label>Name<input type="text" bind:value={$user.name} /></label>
+    </p>
+    <p>
+        <label>E-mail<input type="text" bind:value={$user.email} /></label>
+    </p>
+    <p>
+        <label>Telephone<input type="text" bind:value={$user.phone} /></label>
+    </p>
 
-        <input type="text" bind:value={$user.name} />
-    </label>
-    <label>E-mail
-
-        <input type="text" bind:value={$user.email} />
-    </label>
-    <label>Telefone
-
-        <input type="text" bind:value={$user.phone} />
-    </label>
     <button on:click|preventDefault={submitForm}>Reach out</button>
-  </form>
-<p>
-</p>
+</form>
 
-  <script>
-      import { user } from "../store.js";
+<script>
+    import { user } from "../store.js";
 
-      let submitForm =(event)=>{
-          event.preventDefault();
-          var oReq = new XMLHttpRequest();
-        oReq.open('POST','#');
-        oReq.send($user);
-          // Define what happens on successful data submission
+    let submitForm =(event)=>{
+        event.preventDefault();
+        var oReq = new XMLHttpRequest();
+       
+
         oReq.addEventListener( 'load', function( event ) {
             alert( 'Yeah! Data sent and response loaded.' );
         } );
@@ -33,18 +28,20 @@
         oReq.addEventListener(' error', function( event ) {
             alert( 'Oops! Something went wrong.' );
         } );
-        return false;
 
-      };
-  </script>
+        oReq.open('POST','#');
+        oReq.send($user);
 
-  <style>
-      form{
-          max-width:30%;
-          margin:0 auto;
-      }
-      p>label{
-          display: flex;
-          justify-content: space-between;
-      }
-  </style>
+    };
+</script>
+
+<style>
+    form{
+        max-width:30%;
+        margin:0 auto;
+    }
+    p>label{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
