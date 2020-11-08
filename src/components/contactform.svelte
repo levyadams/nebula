daa48c5f67247831863995f9da262b774fb64077
levyadams <letitslide87@gmail.com>
2020-11-07 23:57:31 -0500

    ads'

--- src/components/contactform.svelte
+++ src/components/contactform.svelte
@@ -1,18 +1,31 @@
-<form name="contact" method="POST" netlify data-netlify="true">
-    <p>
-      <label>Your Name/Organization: <input type="text" name="name" /></label>   
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-  
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Reach out</button>
-    </p>
+<form class="content">
+    <label>Nome
+
+        <input type="text" bind:value={$user.name} />
+    </label>
+    <label>E-mail
+
+        <input type="text" bind:value={$user.email} />
+    </label>
+    <label>Telefone
+
+        <input type="text" bind:value={$user.phone} />
+    </label>
+    <button on:submit|preventDefault={(e)=>{submitForm(e)}}>Reach out</button>
   </form>
+<p>
+</p>
+
+  <script>
+      import { user } from "../store.js";
+
+      let submitForm =(el)=>{
+          var oReq = new XMLHttpRequest();
+        oReq.open('POST','/');
+        oReq.send(($user, 0, 2));
+
+      };
+  </script>
 
   <style>
       form{
